055985dab4df0a6c1195bd7c060caafcdfec3632
edwin <eddiendiritu@gmail.com>
2022-12-22 15:33:09 +0300

    admin features

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
   
 use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\DepartmentsController;
+use App\Http\Controllers\RolesController;
+use App\Http\Controllers\StaffController;
 
-  
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,8 +18,25 @@ Route::controller(RegisterController::class)->group(function(){
     Route::post('register', 'register');
     Route::post('login', 'login');
 });
-   
-     
+
+Route::controller(RolesController::class)->group(function(){
+    Route::get('roles/{id?}','index');
+    Route::post('roles','create');
+    Route::delete('roles/{id}/','destroy');
+});
+Route::controller(DepartmentsController::class)->group(function(){
+    Route::get("departments/{id?}",'index');
+    
+    Route::post('departments','create');
+    Route::patch('departments/{id?}','update');
+});
+Route::controller(StaffController::class) -> group(function(){
+    Route::get("staff/{id?}",'index');
+    Route::post('staff','create');
+    Route::delete('staff/{id?}','destroy');
+    Route::put('staff/{id?}','update');
+
+});
 Route::middleware('auth:sanctum')->group( function () {
     //
 });
